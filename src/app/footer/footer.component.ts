a4df789260ca362e717548908ccdd13f35c78848
FiNkacho <sabaxuxunashvili@gmail.com>
2018-10-30 20:28:18 +0400

    Footer Component

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onUpClick(){
+    window.scrollTo({ top: 0, behavior:"smooth"});
+  }
+
+}
