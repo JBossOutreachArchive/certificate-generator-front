4c3a3b5018bd7aba1adb297d98035a75946d5542
Ananthavijay <vj35.cool@gmail.com>
2018-11-02 06:52:08 +0530

    Update AuthGuard.ts

--- src/app/AuthGuard.ts
+++ src/app/AuthGuard.ts
@@ -1,15 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { catchError, map} from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private loginService:LoginService, private router:Router) { }
 
-    canActivate(next:ActivatedRouteSnapshot, state:RouterStateSnapshot) {
-        return this.loginService.isLoggedIn().map(e => {
-            if (e) {
-                return true;
-            }
-        }).catch(() => {
-            this.router.navigate(['/login']);
-            return Observable.of(false);
-        });
-    }   
+  constructor(private loginService: LoginService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>  {
+    return this.loginService.isLoggedIn().pipe(
+      map(e => {
+        if (e) {
+          return true;
+        } else {
+          ...
+        }
+      }),
+      catchError((err) => {
+        this.router.navigate(['/login']);
+        return of(false);
+      })
+    );
+  }   
+
 }
