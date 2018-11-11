3c97096e6d0ad33621715684a7cf1ff47de89bc8
jamesnoonan <>
2018-11-11 18:33:32 +1000

    Prevent past dates
    Prevent past dates from being entered into the date form on the certificate generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './core/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Certificate Generator';
+  constructor(private authService: AuthService){}
+  ngOnInit() {
+    this.authService.checkIsLoggedIn();    
+  }
 }
