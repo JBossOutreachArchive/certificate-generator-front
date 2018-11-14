6917d63765ab102342be356bdf61f43a7b01c9a2
Ananthavijay <vj35.cool@gmail.com>
2018-11-14 08:02:33 +0530

    Update login.component.ts

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -5,6 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
+
 export class LoginComponent implements OnInit {
 
   constructor() { }
