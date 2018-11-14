89a8dd5e01134c4d868f020aafcc2b52781f8120
Tavleen Sasan <44497420+tavleensasan@users.noreply.github.com>
2018-11-14 19:30:16 +0530

    Create login.component.ts

---
+++ login.component.ts
@@ -0,0 +1,9 @@
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="vendor/select2/select2.min.js"></script>
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+	<script src="js/main.js"></script>
