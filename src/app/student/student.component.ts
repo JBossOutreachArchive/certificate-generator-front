8c6f0268eafefeab8fc56ea33edd44063bc38ac1
Jun Rong <bobo999bobo@outlook.com>
2018-11-21 12:59:41 +0800

    Remove unnecessary classes

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -2,18 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { StudentService } from "../services/student.service"
 import { Observable } from 'rxjs';
 
-class Cert{
-  issuer:string;
-  date:string;
-  name:string;
-}
-
-class ColumnDefs {
-  headerName:string;
-  field:string;
-  sort?:string;
-  comparator?:Function;
-}
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
@@ -21,13 +9,13 @@ class ColumnDefs {
 })
 export class StudentComponent implements OnInit {
 
-  columns: ColumnDefs[];
-  certs: Cert[];
+  columns: any[];
+  certs: any[];
   constructor(private studentService: StudentService) { }
 
   ngOnInit() {
     this.studentService.getAllCertificates()
-    .subscribe((data: Cert[])=>{
+    .subscribe((data: any[])=>{
       data = data.sort((a,b)=>{
         return new Date(b.date).getTime() - new Date(a.date).getTime() 
       })
