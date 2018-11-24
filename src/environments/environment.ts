fd686747df80d5d37d416878369053aee7346bd1
ant159 <44519061+ant159@users.noreply.github.com>
2018-11-24 22:30:52 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:
+ {
+   apiKey: "AIzaSyAmNFEfSoSifOEDiCysjMdzYK3_slXUrS4",
+  authDomain: "certificate-3c777.firebaseapp.com",
+  databaseURL: "https://certificate-3c777.firebaseio.com",
+  projectId: "certificate-3c777",
+  storageBucket: "certificate-3c777.appspot.com",
+  messagingSenderId: "500262041616"
+}
 };
 
 /*
