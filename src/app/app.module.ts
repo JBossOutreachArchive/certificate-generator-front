1d1e75bf1773af15ce5a21d482b92e772b3972a1
ant159 <44519061+ant159@users.noreply.github.com>
2018-11-24 23:10:53 +0200

    Register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {RouterModule, Routes} from "@angular/router";
+import {environment} from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { HttpClientModule } from '@angular/common/http';
+import {AngularFirestore} from 'angularfire2/firestore';
 // Ag grid angular
 import { AgGridModule } from 'ag-grid-angular';
 
 //components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './core/login/login.component';
-import { SignupComponent } from './core/signup/signup.component';
+import { SignupComponent } from './signup/signup.component';
 import { StudentComponent } from './student/student.component';
 import { OrganizationComponent } from './organization/organization.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { BasicComponent } from './templates/basic/basic.component';
-
+const routes: Routes=
+[
+  {path:'signup', component:SignupComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,9 +35,16 @@ import { BasicComponent } from './templates/basic/basic.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+      FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    ReactiveFormsModule,
     AgGridModule.withComponents([])
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
