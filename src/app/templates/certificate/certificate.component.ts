d742201e74490c3cea54aee6e175b7dfabfe662a
Ananthavijay <vj35.cool@gmail.com>
2018-11-25 20:57:09 +0530

    Added certificate template
    changed color

---
+++ src/app/templates/certificate/certificate.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+ @Component({
+  selector: 'app-certificate',
+  templateUrl: './certificate.component.html',
+  styleUrls: ['./certificate.component.css']
+})
+export class CertificateComponent implements OnInit {
+   constructor() { }
+   ngOnInit() {
+  }
+ }
