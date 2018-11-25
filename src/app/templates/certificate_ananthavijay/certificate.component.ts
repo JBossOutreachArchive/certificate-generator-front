39ea65998e8e2a2ec0d2058fdcdcbe6837d8c2e4
Ananthavijay <vj35.cool@gmail.com>
2018-11-25 17:58:04 +0530

    Added certificate template with optional fields

---
+++ src/app/templates/certificate_ananthavijay/certificate.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-certificate',
+  templateUrl: './certificate.component.html',
+  styleUrls: ['./certificate.component.css']
+})
+export class CertificateComponent implements OnInit {
+
+ optional_1 = true;
+ optional_2 = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
