8dadfa27726d8bd0582063aa848125b6a90e0447
Nikita Dhiman <nikitadhmn@gmail.com>
2018-11-25 18:33:24 +0530

    Merge 1
    Final Merge for Login Page.  Includes work from saivittal.

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
+ import { HttpClient, HttpHeaders } from '@angular/common/http';
+ import { User } from './user.service';
+ import { environment } from '../environments/environments';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthenticationService {
+ @Injectable({
+   providedIn: 'root'
+ })
+ export class AuthenticationService {
 
-  constructor() { }
+  
+   readonly baseUrl = environment.baseUrl;
 
-  private getUserInfo(){
-    //authenticates user while login attempt
-  }
+   constructor(private http: HttpClient) { }
 
-  private  signupNewUser(){
-    //this can be a student/organization
-  }
+   login(user: User){
+     debugger;
+     let body = new URLSearchParams();
+     body.set('username', user.username)
+     body.set('password', user.password);
 
-}
+     let options = {
+       headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+                                 .set('Access-Control-Allow-Origin', '*')
+     };
+
+     return this.http.post(this.baseUrl+"api-token-auth/", body.toString(), options)
+   }
+ }
