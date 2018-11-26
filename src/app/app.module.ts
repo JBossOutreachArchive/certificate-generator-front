e6110ed5a616e767afdd9912cd5a64b6c7f621fe
ant159 <44519061+ant159@users.noreply.github.com>
2018-11-26 19:22:05 +0200

    View certificates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from "@angular/router";
 // Ag grid angular
 import { AgGridModule } from 'ag-grid-angular';
 
 //components
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Certificate1Component } from './certificate1/certificate1.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './core/login/login.component';
 import { SignupComponent } from './core/signup/signup.component';
@@ -12,7 +14,11 @@ import { StudentComponent } from './student/student.component';
 import { OrganizationComponent } from './organization/organization.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { BasicComponent } from './templates/basic/basic.component';
-
+const routes: Routes=
+[
+  {path:'dashboard', component:DashboardComponent},
+  {path:'certificate1', component:Certificate1Component}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,11 +26,14 @@ import { BasicComponent } from './templates/basic/basic.component';
     SignupComponent,
     StudentComponent,
     OrganizationComponent,
+    DashboardComponent,
+    Certificate1Component,
     BasicComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+      RouterModule.forRoot(routes),
     AgGridModule.withComponents([])
   ],
   providers: [],
