204a80e885c3bdb85dd0478fbb7fcd1db41c7eab
FiNkacho <sabaxuxunashvili@gmail.com>
2018-11-26 21:38:13 +0400

    fix merge conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
