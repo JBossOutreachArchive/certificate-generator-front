f12beea12a604a9b69716e3ccb12ee1d6396db0b
Sidhya Tikku <sidhyaktikku@gmail.com>
2018-12-08 21:38:56 +0530

    studdd
    Merge branch 'master' into studd
    err
    
    Merge branch 'master' into studd
    Delete package-lock.json
    Merge branch 'master' into studd
    Update student.component.css
    
    Update student.component.css

---
+++ src/app/templates/certificate-geek/certificate-gci18.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+ @Component({
+  selector: 'app-basic',
+  templateUrl: './certificate-gci18.component.html',
+  styleUrls: ['./certificate-gci18.component.css']
+})
+export class CertificateGCIComponent implements OnInit {
+   constructor() { }
+   ngOnInit() {
+  }
+ }
