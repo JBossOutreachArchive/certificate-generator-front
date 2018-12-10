34a632e825dc5ac8c3c510973567b9702afee839
Aayushman Choudhary <Aayushmanchaudhory@gmail.com>
2018-12-10 21:10:04 +0530

    Added Optional field.

--- src/app/templates/certificate-geek/certificate-gci18.component.ts
+++ src/app/templates/certificate-geek/certificate-gci18.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
  @Component({
-  selector: 'app-basic',
+  selector: 'app-cert-geek',
   templateUrl: './certificate-gci18.component.html',
   styleUrls: ['./certificate-gci18.component.css']
 })
 export class CertificateGCIComponent implements OnInit {
+   addValidTill = true;
    constructor() { }
    ngOnInit() {
   }
