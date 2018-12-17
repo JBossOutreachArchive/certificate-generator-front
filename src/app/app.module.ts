12c3963a6e542305534fe6da6704f2e918478707
ant159 <44519061+ant159@users.noreply.github.com>
2018-12-17 21:38:05 +0200

    Customize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 
