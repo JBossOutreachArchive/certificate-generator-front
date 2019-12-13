5afd629be582992b7f5a47c965a9c717483f9cc2
sayam <sawaisayam@gmail.com>
2019-12-14 03:11:09 +0530

    Integrated API for Student Dashboard

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,13 +1,28 @@
+// Imports
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Certificate } from './certificate.model';
+import { Environment } from '../environments/environments';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor() { }
+  constructor(private http:HttpClient) {}
 
+  getCertificates():Observable<Certificate[]>{
+    const token = 'JWT ' + localStorage.getItem('token');
+    const options = {
+      headers: new HttpHeaders().set('Authorization', token)
+    };
+    return this.http.get<Certificate[]>(Environment.baseUrl+"api/student/certificates/",options);
+
+  }
+  /*
   private getAllCertificates() {
     // Fetches all the certificates issued to student
-  }
+  }*/
 }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,14 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../services/student.service';
+import { Certificate } from '../services/certificate.model';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
+
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  private gridApi;
+
+  constructor(private studentService:StudentService) { }
+
+  columnDefs = [
+    {headerName: 'Organization Name', field:'name',checkboxSelection: true},
+    {headerDate: 'Date',field:'date'},
+    {headerName: 'Certificate Description', field: 'desc'}
+  ];
+
+  rowData = [];
 
   ngOnInit() {
+    
+    this.studentService.getCertificates().subscribe((certificates: Certificate[]) =>{
+
+      // add certificates in ag-grid
+      for(const certificate of certificates){
+        this.rowData.push({name: certificate.issuing_organization.name, 
+                           date: certificate.date,
+                           desc: certificate.issued_for
+                          });
+      }
+      this.gridApi.setRowData(this.rowData);
+    })
+  }
+
+  onGridReady(event) {
+    this.gridApi = event.api;
   }
 }
