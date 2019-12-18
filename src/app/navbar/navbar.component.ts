6e5a7b7fda7abb021b5cb5a3cb0b31856481943e
sayam <sawaisayam@gmail.com>
2019-12-18 23:46:28 +0530

    Added Username to Navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,6 +10,19 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+  username:string;
+
+  isLoggedin(){
+
+    const name_exists = localStorage.getItem('username');
+    if(name_exists){
+      this.username = name_exists;
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
   ngOnInit() {
   }
 
