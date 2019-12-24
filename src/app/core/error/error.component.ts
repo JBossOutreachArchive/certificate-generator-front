5ed6a9ec89852dce3901463502f0ec84c1581e16
ParthPali <54657955+ParthPali@users.noreply.github.com>
2019-12-23 20:56:08 -0800

    Add files via upload

---
+++ src/app/core/error/error.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.css']
+})
+export class ErrorComponent {
+
+  constructor(
+    private mAuth: AuthenticationService,
+    private router: Router
+  ) { }
+}
