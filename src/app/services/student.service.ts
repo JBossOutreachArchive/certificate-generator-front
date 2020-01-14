1706fc8424947e5ea1285e620acfac3b64b0f8f8
reaganiwadha <reaganiwadha@protonmail.com>
2020-01-14 07:36:47 +0700

    Added Student Dashboard Integration #1
    Integrated student dashboard

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Certificate } from './certificate.model';
+import {Observable} from 'rxjs';
+import {Environment as env} from '../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
-  private getAllCertificates() {
+  getAllCertificates() :  Observable<Certificate[]> {
     // Fetches all the certificates issued to student
+    return this.httpClient.get<Certificate[]>(env.baseUrl+"api/get_certificates",{
+      headers: new HttpHeaders().set('Authorization', ("JWT " + localStorage.getItem('token')))
+    })
   }
+
 }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../services/student.service';
+import { Certificate } from '../services/certificate.model';
 
 @Component({
   selector: 'app-student',
@@ -6,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
+  private studentName;
+  private gridApi;
+  private noCertificates;
+  
+  constructor(private studentService: StudentService) { }
 
-  constructor() { }
+  columnDefs = [
+    {headerName: 'Organization Name', field: 'name'},
+    {headerName: 'Date', field: 'date' },
+    {headerName: 'Certificate Description', field: 'desc'}
+  ];
+
+  rowData = [];
 
   ngOnInit() {
+    this.studentService.getAllCertificates().subscribe((certificates: Certificate[]) => {
+      this.noCertificates = certificates.length === 0;
+
+      if(certificates.length != 0){
+        this.studentName = certificates[0].student.name;
+        certificates.forEach(c => {
+          this.rowData.push({
+            name: c.issuing_organization.name, 
+            date: c.date,
+            desc: c.issued_for
+          });
+        });
+        this.gridApi.setRowData(this.rowData);
+      }
+    })
   }
+
+  onGridReady(event){
+    this.gridApi = event.api;
+  }
+
 }
